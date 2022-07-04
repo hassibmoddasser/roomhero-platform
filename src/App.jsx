d239bf2702f9dcd4b0cb780f697453954e1c9c3e
Hassib Moddasser <hassib.moddasser@gmail.com>
2022-07-04 19:12:05 +0430

    refactor: add routers and update jsx in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import React, { useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Header from "./components/layout/Header";
+import Sidebar from "./components/layout/Sidebar";
+
+import Dashboard from "./components/pages/Dashboard";
+import Supplier from "./components/pages/Supplier";
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <Router>
+      <div>
+        <Sidebar />
+
+        <div className="flex flex-col h-screen md:pl-64">
+          <Header setSidebarOpen={setSidebarOpen} />
+
+          <main className="flex-1 bg-white">
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route exact path="/supplier" element={<Supplier />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
